44b05f0d65f9cb6a90d1ce148ddd0625fd6a26ed
Sean Reed <reeds@valassis.com>
2018-04-22 22:52:58 -0700

    Pushing POST to ES

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
 import base64
 import json
 import snappy
+import time
 
 from flask import Flask, request
+from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
+
+class ElasticSearchSink(object):
+
+    def __init__(self, host='127.0.0.1:9300'):
+        self.es = Elasticsearch()
+
+    def send(self, blobs):
+        for blob in blobs:
+            todaysIndex = '%s-%s' % (blob['type'], self.get_index())
+            res = self.es.index(index=todaysIndex, doc_type=blob['type'], body=blob)
+            print(res['_id'] + ': ' + repr(blob))
+
+        return True
+
+
+    def get_index(self):
+        day = time.strftime("%d-%m-%Y")
+
+        return 'hlds-%s' % day
+
+
+def send_to_es(blobs):
+    es = ElasticSearchSink()
+
+    return es.send(blobs)
+
 
 @app.route("/", methods=['POST'])
 def blob_ingest():
@@ -16,6 +44,9 @@ def blob_ingest():
 
     logs = snappy.decompress(base64ed)
 
-    print(json.loads(logs))
+    blobs = json.loads(logs)
 
-    return "test"
+    if send_to_es(blobs):
+        return "sent"
+    else:
+        return "Error"
